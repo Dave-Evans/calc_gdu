30799dda7bb835062d3acdd51a51c9ad0fbb37fd
Dave-Evans <evans.dave.michael@gmail.com>
2023-11-17 14:01:16 -0600

    reformatted program so that geopandas and pandas is not needed to slim down deployment

--- README.md
+++ README.md
@@ -9,15 +9,51 @@ I'm creating a separate repository from main web app.
 `mkdir calc_gdu`
 
 Get latest(?) version of python for lambda compatibility.
-Create a virtual environment and install `requests` and `geopandas`.
+Create a virtual environment and install `requests` # and `geopandas`.
 
 ```bash
 sudo apt update
 sudo apt install python3.11
 sudo apt install python3.11-dev python3.11-venv
-python3.11 -m venv lenv
-source lenv/bin/activate
-pip install requests geopandas
+python3.11 -m venv denv
+source denv/bin/activate
+pip install requests # geopandas
+
+cd denv/lib/python3.11/site-packages
+zip -r ../../../../dep_pkg.zip .
+
+cd ../../../..
+zip dep_pkg.zip gdu_calc.py
+```
+
+
+For testing lambda
+```bash
+curl -v 'https://c76d5gzlin46g55ymdroqm24lu0efpnp.lambda-url.us-east-2.on.aws/' \
+-H 'content-type: application/json' \
+-d '{ "lon": -96.80417, "lat": 45.5948, "start_date": "2020-08-18", "end_date": "2021-04-19"}'
+
+# 2785
+curl -v 'https://c76d5gzlin46g55ymdroqm24lu0efpnp.lambda-url.us-east-2.on.aws/' \
+-H 'content-type: application/json' \
+-d '{ "lon": -89.2988646, "lat": 43.0899635, "start_date": "2020-06-21", "end_date": "2020-09-21"}'
+
+# 2886.8
+curl -v 'https://c76d5gzlin46g55ymdroqm24lu0efpnp.lambda-url.us-east-2.on.aws/' \
+-H 'content-type: application/json' \
+-d '{ "lon": -89.2988646, "lat": 43.0899635, "start_date": "2021-06-21", "end_date": "2021-09-21"}'
+
+# 2922
+curl -v 'https://c76d5gzlin46g55ymdroqm24lu0efpnp.lambda-url.us-east-2.on.aws/' \
+-H 'content-type: application/json' \
+-d '{ "lon": -89.2988646, "lat": 43.0899635, "start_date": "2022-06-21", "end_date": "2022-09-21"}'
+
+# 2883
+curl -v 'https://c76d5gzlin46g55ymdroqm24lu0efpnp.lambda-url.us-east-2.on.aws/' \
+-H 'content-type: application/json' \
+-d '{ "lon": -89.2988646, "lat": 43.0899635, "start_date": "2023-06-21", "end_date": "2023-09-21"}'
+
+
 ```
 
 
--- gdu_calc.py
+++ gdu_calc.py
@@ -1,6 +1,8 @@
 import requests
-import geopandas
-from shapely.geometry import Point
+import math
+
+# import geopandas
+# from shapely.geometry import Point
 import datetime
 import json
 import logging
@@ -15,6 +17,7 @@ def lambda_handler(event, context):
 
     print("Body:")
     body = json.loads(event["body"])
+    print(body)
 
     start_date = body["start_date"]
     end_date = body["end_date"]
@@ -22,13 +25,20 @@ def lambda_handler(event, context):
     lat = body["lat"]
     base_number = 40
     upper_thresh = 86
-    gdu = calc_gdu(start_date, end_date, lon, lat, base_number=40, upper_thresh=86)
-    print(f"The area is {gdu}")
+    gdu = calc_gdu(
+        start_date,
+        end_date,
+        lon,
+        lat,
+        base_number=base_number,
+        upper_thresh=upper_thresh,
+    )
+    print(f"The gdu: {gdu}")
 
     logger.info(f"CloudWatch logs group: {context.log_group_name}")
 
     # return the calculated area as a JSON string
-    data = {"area": area}
+    data = {"gdu": gdu}
     return json.dumps(data)
 
 
@@ -45,36 +55,51 @@ def get_stations():
         list_stations.extend(resp)
 
     # convert to geospatial
-    df_stations = geopandas.pd.DataFrame(list_stations)
-    df_stations = geopandas.GeoDataFrame(
-        df_stations,
-        geometry=geopandas.points_from_xy(
-            df_stations.stationlongitude, df_stations.stationlatitude
-        ),
-        crs=4326,
+    # df_stations = geopandas.pd.DataFrame(list_stations)
+    # df_stations = geopandas.GeoDataFrame(
+    #     df_stations,
+    #     geometry=geopandas.points_from_xy(
+    #         df_stations.stationlongitude, df_stations.stationlatitude
+    #     ),
+    #     crs=4326,
+    # )
+
+    return list_stations
+
+
+def calc_dist(lon1, lat1, lon2, lat2):
+    """For calculating the distance between two points
+    powerappsguide.com/blog/post/formulas-calculate-the-distance-between-2-points-longitude-latitude
+    returns distance in km
+    """
+    r = 6371  # radius of Earth (KM)
+    p = 0.017453292519943295  # Pi/180
+    a = (
+        0.5
+        - math.cos((lat2 - lat1) * p) / 2
+        + math.cos(lat1 * p)
+        * math.cos(lat2 * p)
+        * (1 - math.cos((lon2 - lon1) * p))
+        / 2
     )
 
-    return df_stations
+    d = 2 * r * math.asin(math.sqrt(a))  # 2*R*asin
+
+    return d
 
 
 # find nearest station to covercrop location
-def get_dist_to_stations(df_stations, df_collection):
-    df_stations = df_stations.to_crs(epsg=5070)
+def get_dist_to_stations(list_stations, lon, lat):
+    """Calculate distance to point from each station"""
+    for station in list_stations:
+        station["distance"] = calc_dist(
+            station["stationlongitude"], station["stationlatitude"], lon, lat
+        )
 
-    ## duplication records in collection to work with
-    ##  distance function
-    dupe_collection = df_collection.copy()
-    for i in range(1, len(df_stations)):
-        dupe_collection = geopandas.pd.concat([dupe_collection, df_collection])
-    dupe_collection = dupe_collection.to_crs(epsg=5070)
+    # sort by distance, smallest to largest
+    list_stations = sorted(list_stations, key=lambda d: d["distance"])
 
-    dupe_collection.reset_index(inplace=True, drop=True)
-    dist = dupe_collection.distance(df_stations, align=True)
-    dist.name = "distance"
-    df_station_distance = df_stations.join(dist)
-    df_station_distance.sort_values("distance", inplace=True)
-
-    return df_station_distance
+    return list_stations
 
 
 # collect data from that station
@@ -89,18 +114,18 @@ def retrieve_station_data(stationid, start_date, end_date):
     headers = {"Accept": "application/json"}
 
     reductions = "avg"
-    # payload = "start={start_date}&end={end_date}&elem={element}&interval={interval}&reduction={reduction}".format(
-    #     start_date=start_date,
-    #     end_date=end_date,
-    #     element=element,
-    #     interval=interval,
-    #     reduction=reductions,
-    # )
-    payload = "start={start_date}&end={end_date}&elem={element}".format(
+    payload = "start={start_date}&end={end_date}&elem={element}&interval={interval}&reduction={reduction}".format(
         start_date=start_date,
         end_date=end_date,
         element=element,
+        interval=interval,
+        reduction=reductions,
     )
+    # payload = "start={start_date}&end={end_date}&elem={element}".format(
+    #     start_date=start_date,
+    #     end_date=end_date,
+    #     element=element,
+    # )
 
     resp = requests.get(
         url_station_collect.format(stationid=stationid, query_string=payload),
@@ -110,26 +135,10 @@ def retrieve_station_data(stationid, start_date, end_date):
     if resp == {}:
         print("No data.")
         return None
-    df = geopandas.pd.DataFrame(resp).transpose()
 
-    df.AVA = geopandas.pd.to_numeric(df.AVA, errors="coerce")
+    # What do nulls look like?
 
-    prop_missing = df.AVA.isna().sum() / len(df)
-    # print(f"{round(prop_missing,3) *100} missing")
-
-    if prop_missing > 0.05:
-        return None
-
-    df["collect_time"] = geopandas.pd.to_datetime(df.index, format="%Y%m%d%H%M")
-
-    grpd = df.groupby(geopandas.pd.Grouper(key="collect_time", axis=0, freq="D")).agg(
-        min_temp=geopandas.pd.NamedAgg(column="AVA", aggfunc="min"),
-        max_temp=geopandas.pd.NamedAgg(column="AVA", aggfunc="max"),
-        avg_temp=geopandas.pd.NamedAgg(column="AVA", aggfunc="mean"),
-        count_obs=geopandas.pd.NamedAgg(column="AVA", aggfunc="count"),
-    )
-
-    return grpd
+    return resp
 
 
 # Calculate growing degree days
@@ -143,57 +152,51 @@ def calc_gdu(start_date, end_date, lon, lat, base_number=40, upper_thresh=86):
     if end_date <= start_date:
         return None
 
-    df_collection = geopandas.GeoDataFrame(
-        {
-            # "photo_taken_date": [datetime.date(2022, 6, 6)],
-            # "cover_crop_planting_date": [datetime.date(2021, 10, 4)],
-            # "geometry": [Point((-96.55012607574463, 45.8826454738681))],
-            "photo_taken_date": [end_date],
-            "cover_crop_planting_date": [start_date],
-            "geometry": [Point(lon, lat)],
-        },
-        crs=4326,
-    )
-
     # gets all stations
-    df_stations = get_stations()
-
+    logging.info("About to get stations.")
+    list_stations = get_stations()
+    logging.info("Received " + str(len(list_stations)))
     # get df of distance to all stations
-    df_station_distance = get_dist_to_stations(df_stations, df_collection)
+    logging.info("Calculating distance from point to all stations")
+    list_stations = get_dist_to_stations(list_stations, lon, lat)
+    logging.info("Closest station is:" + list_stations[0]["weabaseid"])
 
     attempt = 0
     while True:
-        closest_station = df_station_distance.iloc[attempt]
-        stationid = closest_station.weabaseid
+        closest_station = list_stations[attempt]
+        stationid = closest_station["weabaseid"]
 
-        df_station_data = retrieve_station_data(
+        station_data = retrieve_station_data(
             stationid,
             start_date.strftime("%Y%m%d"),
             end_date.strftime("%Y%m%d"),
         )
 
-        if df_station_data is None:
+        if station_data is None:
             attempt += 1
         else:
             break
 
-    # If avg_temp is NA
-    # then set to 0 (this was screwing up the following where statement)
-    df_station_data.avg_temp = df_station_data.avg_temp.where(
-        ~geopandas.pd.isna(df_station_data.avg_temp), 0
-    )
+    cumulative_gdd = 0
+    for i in station_data:
+        print("Before:", station_data[i]["AVA"])
+        # Set NA in avg_temp to 0, what's this look like?
+        # station_data[i]['AVA']
 
-    # If greater than upper_thresh (86), set to upper_thresh (86)
-    df_station_data.avg_temp = df_station_data.avg_temp.where(
-        df_station_data.avg_temp < upper_thresh, upper_thresh
-    )
+        # Set upper threshold to 86
+        if station_data[i]["AVA"] > 86:
+            station_data[i]["AVA"] = 86
 
-    gdd = df_station_data.avg_temp - base_number
+        # subtract base number from avg temp
+        station_data[i]["AVA"] = station_data[i]["AVA"] - base_number
 
-    # Where greater or equal to 0 keep; otherwise replace with 0
-    gdd.where(gdd >= 0, 0, inplace=True)
+        # Set less than 0 to 0
+        if station_data[i]["AVA"] < 0:
+            station_data[i]["AVA"] = 0
 
-    cumulative_gdd = gdd.sum()
+        print("After:", station_data[i]["AVA"])
+
+        cumulative_gdd += station_data[i]["AVA"]
 
     return cumulative_gdd
 
