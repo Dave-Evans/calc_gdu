2c4aba3fc7f0dcdfef48a5b2bdf5a8ccc2fa8e7f
Dave-Evans <evans.dave.michael@gmail.com>
2023-11-22 08:00:12 -0600

    wow, functional terraform code for deploying the lambda!

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "project_name" {
+  type        = map(any)
+  description = "Name of the project."
+  default = {
+    dev     = "davemike-dev"
+    prod    = "davemike-prod"
+    scratch = "davemike-scratch"
+  }
+}
+
+variable "lambda_root" {
+  type        = string
+  description = "The relative path to the source of the lambda"
+  default     = "../calc_gdu"
+}
+
+
+variable "env" {
+  description = "env: dev or prod"
+}
+
+variable "aws_s3_region_name" {
+  default = "us-east-2"
+}
+
+variable "aws_access_key_id" {
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Access Key"
+}
+
+variable "aws_storage_bucket_name" {
+  description = "The name of the primary app bucket"
+}
+
+variable "aws_backup_bucket_name" {
+  description = "The name of the bucket to backup the db"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Name of the private key"
+}
+
+
+
